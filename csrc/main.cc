fba52afcfda79dc2628a1ddf5c384bbc52ec8d54
Deependu <deependujha21@gmail.com>
2025-01-04 13:25:10 +0530

    Feat: complete value object (#7)
    * Value class supports add, subtract, multiply, power, relu and back-propagation
    
    * replace raw pointers with smart pointers and pytest also passes
    
    * minor fix
    
    * update

--- csrc/main.cc
+++ csrc/main.cc
@@ -1,11 +1,72 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include "value.h"
 
-int add(int i, int j) {
-  return i + j;
-}
+namespace py = pybind11;
+using overload_cast_ = pybind11::detail::overload_cast_impl<Value>;
 
 PYBIND11_MODULE(_core, m) {
-  m.doc() = "pybind11 example plugin"; // optional module docstring
-
-  m.def("add", &add, "A function that adds two numbers");
+  m.doc() =
+      "A minimal deep learning framework made by Deependu Jha <deependujha21@gmail.com>"; // optional module docstring
+  py::class_<Value, std::shared_ptr<Value>>(m, "Value")
+      .def(py::init<double>())
+      .def(py::init<double, std::unordered_set<std::shared_ptr<Value>>, char>())
+      .def_readwrite("data", &Value::data)
+      .def_readwrite("grad", &Value::grad)
+      .def_readwrite("_prev", &Value::_prev)
+      .def_readwrite("char", &Value::_op)
+      .def("backward", &Value::backward)
+      .def("executeBackward", &Value::executeBackWardMethod)
+      .def("__repr__", &Value::printMe)
+      .def(
+          "__add__",
+          static_cast<std::shared_ptr<Value> (Value::*)(double)>(&Value::add),
+          "add value object with double")
+      .def(
+          "__radd__",
+          static_cast<std::shared_ptr<Value> (Value::*)(double)>(&Value::add),
+          "add value object with double")
+      .def(
+          "__add__",
+          static_cast<std::shared_ptr<Value> (Value::*)(
+              std::shared_ptr<Value>)>(&Value::add),
+          "add value object with value object")
+      .def(
+          "__sub__",
+          static_cast<std::shared_ptr<Value> (Value::*)(double)>(&Value::sub),
+          "subtract value object with double")
+      .def(
+          "__rsub__",
+          static_cast<std::shared_ptr<Value> (Value::*)(double)>(&Value::sub),
+          "subtract value object with double")
+      .def(
+          "__sub__",
+          static_cast<std::shared_ptr<Value> (Value::*)(
+              std::shared_ptr<Value>)>(&Value::mul),
+          "subtract value object with value object")
+      .def(
+          "__mul__",
+          static_cast<std::shared_ptr<Value> (Value::*)(double)>(&Value::mul),
+          "multiply value object with double")
+      .def(
+          "__rmul__",
+          static_cast<std::shared_ptr<Value> (Value::*)(double)>(&Value::mul),
+          "multiply value object with double")
+      .def(
+          "__mul__",
+          static_cast<std::shared_ptr<Value> (Value::*)(
+              std::shared_ptr<Value>)>(&Value::mul),
+          "multiply value object with value object")
+      .def(
+          "__pow__",
+          static_cast<std::shared_ptr<Value> (Value::*)(int)>(&Value::pow),
+          "raise power of value object by int n")
+      .def(
+          "__neg__",
+          static_cast<std::shared_ptr<Value> (Value::*)()>(&Value::neg),
+          "negative of the value object")
+      .def(
+          "relu",
+          static_cast<std::shared_ptr<Value> (Value::*)()>(&Value::relu),
+          "apply relu operation");
 }
--- ctests/value_fixture_test.cc
+++ ctests/value_fixture_test.cc
@@ -1,24 +1,22 @@
 #include <gtest/gtest.h>
+#include <memory>
 #include "value.h"
 
 // `Value fixture` for googletest
 class ValueTest : public testing::Test {
 protected:
-  ValueTest() {
-    this->v1 = new Value(5.1);
-    this->v2 = new Value(4.2);
-  }
+  ValueTest()
+      : v1(std::make_shared<Value>(5.1)), v2(std::make_shared<Value>(4.2)) {}
 
   //   ~ValueTest() override = default;
 
-  Value* v1 = new Value(5.1);
-  Value* v2 = new Value(4.2);
+  std::shared_ptr<Value> v1, v2;
 };
 
 // ========= TESTs =========
 
 TEST_F(ValueTest, ValueAddition) {
-  Value* v3 = v1->add(v2);
+  std::shared_ptr<Value> v3 = v1->add(v2);
 
   EXPECT_DOUBLE_EQ(v1->data, double(5.1));
   EXPECT_DOUBLE_EQ(v2->data, double(4.2));
@@ -35,12 +33,10 @@ TEST_F(ValueTest, ValueAddition) {
   EXPECT_DOUBLE_EQ(v1->grad, double(-2.3));
   EXPECT_DOUBLE_EQ(v2->grad, double(-2.3));
   EXPECT_DOUBLE_EQ(v3->grad, double(-2.3));
-
-  delete v3;
 }
 
 TEST_F(ValueTest, ValueSubtraction) {
-  Value* v3 = v1->sub(v2);
+  std::shared_ptr<Value> v3 = v1->sub(v2);
 
   EXPECT_DOUBLE_EQ(v1->data, double(5.1));
   EXPECT_DOUBLE_EQ(v2->data, double(4.2));
@@ -57,12 +53,10 @@ TEST_F(ValueTest, ValueSubtraction) {
   EXPECT_DOUBLE_EQ(v1->grad, double(-2.3));
   EXPECT_DOUBLE_EQ(v2->grad, double(2.3));
   EXPECT_DOUBLE_EQ(v3->grad, double(-2.3));
-
-  delete v3;
 }
 
 TEST_F(ValueTest, ValueMultiplication) {
-  Value* v3 = v1->mul(v2);
+  std::shared_ptr<Value> v3 = v1->mul(v2);
 
   EXPECT_DOUBLE_EQ(v1->data, double(5.1));
   EXPECT_DOUBLE_EQ(v2->data, double(4.2));
@@ -80,12 +74,10 @@ TEST_F(ValueTest, ValueMultiplication) {
   EXPECT_DOUBLE_EQ(v1->grad, double(-2.3 * v2->data));
   EXPECT_DOUBLE_EQ(v2->grad, double(-2.3 * v1->data));
   EXPECT_DOUBLE_EQ(v3->grad, double(-2.3));
-
-  delete v3;
 }
 
 TEST_F(ValueTest, ValuePower) {
-  Value* v3 = v1->pow(3);
+  std::shared_ptr<Value> v3 = v1->pow(3);
 
   EXPECT_DOUBLE_EQ(v1->data, double(5.1));
   EXPECT_DOUBLE_EQ(v3->data, double(5.1 * 5.1 * 5.1));
@@ -100,12 +92,10 @@ TEST_F(ValueTest, ValuePower) {
   // differentiation of f(x,y) = x * y => x+y
   EXPECT_DOUBLE_EQ(v1->grad, double(-2.3 * 3 * v1->data * v1->data));
   EXPECT_DOUBLE_EQ(v3->grad, double(-2.3));
-
-  delete v3;
 }
 
 TEST_F(ValueTest, ValueRelu) {
-  Value* v3 = v1->relu();
+  std::shared_ptr<Value> v3 = v1->relu();
 
   EXPECT_DOUBLE_EQ(v1->data, double(5.1));
   EXPECT_DOUBLE_EQ(v3->data, double(5.1));
@@ -121,12 +111,10 @@ TEST_F(ValueTest, ValueRelu) {
   EXPECT_DOUBLE_EQ(v1->grad, double(-2.3));
   EXPECT_DOUBLE_EQ(v3->grad, double(-2.3));
 
-  delete v3;
-
   // ======= relu second case =======
 
-  v3 = new Value(-5.2);
-  Value* v4 = v3->relu();
+  v3 = std::make_shared<Value>(-5.2);
+  std::shared_ptr<Value> v4 = v3->relu();
 
   EXPECT_DOUBLE_EQ(v3->data, double(-5.2));
   EXPECT_DOUBLE_EQ(v4->data, double(0));
@@ -141,17 +129,14 @@ TEST_F(ValueTest, ValueRelu) {
   // differentiation of f(x,y) = x * y => x+y
   EXPECT_DOUBLE_EQ(v3->grad, double(0));
   EXPECT_DOUBLE_EQ(v4->grad, double(-2.3));
-
-  delete v3;
-  delete v4;
 }
 
 TEST_F(ValueTest, CombinedTest) {
   // f(y) = x**2 + 5*y - 1
-  Value* x = new Value(7.0);
-  Value* y = new Value(2.0);
+  std::shared_ptr<Value> x = std::make_shared<Value>(7.0);
+  std::shared_ptr<Value> y = std::make_shared<Value>(2.0);
 
-  Value* z = x->pow(2)->add(y->mul(5))->sub(1);
+  std::shared_ptr<Value> z = x->pow(2)->add(y->mul(5))->sub(1);
 
   EXPECT_DOUBLE_EQ(x->data, double(7.0));
   EXPECT_DOUBLE_EQ(y->data, double(2.0));
