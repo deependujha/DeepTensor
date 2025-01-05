49db1b91f5da884d6b7895c101d596ca2ea2cb31
Deependu Jha <deependujha21@gmail.com>
2025-01-05 16:17:18 +0530

    update

--- src/deeptensor/__init__.py
+++ src/deeptensor/__init__.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from deeptensor.__about__ import *  # noqa: F403
-
 from ._core import MLP, Layer, Neuron, Value, __doc__
 
 __all__ = ["MLP", "Layer", "Neuron", "Value", "__doc__"]
