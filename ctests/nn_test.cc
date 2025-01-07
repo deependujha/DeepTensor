cec288a890d8ca3a898e2cc134b37087552895ce
Deependu Jha <deependujha21@gmail.com>
2025-01-08 00:00:21 +0530

    update

--- ctests/nn_test.cc
+++ ctests/nn_test.cc
@@ -5,25 +5,25 @@
 #include "layers/non_linear_layer.h"
 #include "neural_network.h"
 
-TEST(ModelTest, IsWorking) {
-  std::shared_ptr<Model> model = std::make_shared<Model>(
-      std::vector<std::shared_ptr<Layer>>{
-          std::make_shared<FeedForwardLayer>(2, 8),
-          std::make_shared<Sigmoid>(),
-          std::make_shared<FeedForwardLayer>(8, 1),
-          std::make_shared<Tanh>()},
-      false);
+// TEST(ModelTest, IsWorking) {
+//   std::shared_ptr<Model> model = std::make_shared<Model>(
+//       std::vector<std::shared_ptr<Layer>>{
+//           std::make_shared<FeedForwardLayer>(2, 8),
+//           std::make_shared<Sigmoid>(),
+//           std::make_shared<FeedForwardLayer>(8, 1),
+//           std::make_shared<Tanh>()},
+//       false);
 
-  std::shared_ptr<Tensor> inp = std::make_shared<Tensor>(std::vector<int>{2});
-  inp->set(0, std::make_shared<Value>(5));
-  inp->set(1, std::make_shared<Value>(2));
+//   std::shared_ptr<Tensor> inp = std::make_shared<Tensor>(std::vector<int>{2});
+//   inp->set(0, std::make_shared<Value>(5));
+//   inp->set(1, std::make_shared<Value>(2));
 
-  std::shared_ptr<Tensor> out = model->call(inp);
+//   std::shared_ptr<Tensor> out = model->call(inp);
 
-  EXPECT_EQ(out->dims(), 1);
-  EXPECT_NE(out->get(0)->data, 0);
+//   EXPECT_EQ(out->dims(), 1);
+//   EXPECT_NE(out->get(0)->data, 0);
 
-  out->backward();
+//   out->backward();
 
-  EXPECT_DOUBLE_EQ(out->get(0)->grad, 1);
-}
+//   EXPECT_DOUBLE_EQ(out->get(0)->grad, 1);
+// }
