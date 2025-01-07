d7db19530c8d82545609ecc4e7942054b21ed97c
Deependu Jha <deependujha21@gmail.com>
2025-01-07 23:33:20 +0530

    update

--- ctests/nn_test.cc
+++ ctests/nn_test.cc
@@ -13,12 +13,14 @@ TEST(ModelTest, IsWorking) {
           std::make_shared<FeedForwardLayer>(8, 1),
           std::make_shared<Tanh>()},
       false);
-
+std::cerr<<"model creation done\n";
   std::shared_ptr<Tensor> inp = std::make_shared<Tensor>(std::vector<int>{2});
   inp->set(0, std::make_shared<Value>(5));
   inp->set(1, std::make_shared<Value>(2));
+std::cerr<<"model input done\n";
 
   std::shared_ptr<Tensor> out = model->call(inp);
+std::cerr<<"model output done\n";
 
   EXPECT_EQ(out->dims(), 1);
   EXPECT_NE(out->get(0)->data, 0);
