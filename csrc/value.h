bf86bdf731c0d265a8d03acbc9dc77ef118084c5
Deependu Jha <deependujha21@gmail.com>
2025-01-09 01:57:43 +0530

    update

--- csrc/value.h
+++ csrc/value.h
@@ -6,6 +6,10 @@
 #include <utility>
 #include <vector>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846264338327950288
+#endif
+
 class Value : public std::enable_shared_from_this<Value> {
 private:
   //   internal properties
