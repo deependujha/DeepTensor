56099ba2f5998cf1e257a8cb057f91a40509ad55
Deependu Jha <deependujha21@gmail.com>
2025-01-14 16:38:44 +0530

    update

--- ctests/loss_test.cc
+++ ctests/loss_test.cc
@@ -25,7 +25,8 @@ TEST(LossTest, MSELossTest) {
   EXPECT_DOUBLE_EQ(out->data, expectedValue);
 }
 
-TEST(LossTest, CrossEntropyLossTest) {
+// ===== cross entropy loss =====
+TEST(LossTest, CrossEntropyLossTest1) {
   std::shared_ptr<Tensor> x = std::make_shared<Tensor>(std::vector<int>{3});
   std::shared_ptr<Tensor> y = std::make_shared<Tensor>(std::vector<int>{3});
 
@@ -41,6 +42,50 @@ TEST(LossTest, CrossEntropyLossTest) {
   std::shared_ptr<Value> out = cross_entropy(x, y);
 
   double expectedValue = 0.6220; // calculated using pytorch nn.CrossEntropyLoss
+  double tolerance = 0.0001;    // Compare up to 4 decimal places
+
+
+  EXPECT_NEAR(out->data, expectedValue, tolerance);
+}
+
+TEST(LossTest, CrossEntropyLossTest2) {
+  std::shared_ptr<Tensor> x = std::make_shared<Tensor>(std::vector<int>{3});
+  std::shared_ptr<Tensor> y = std::make_shared<Tensor>(std::vector<int>{3});
+
+  // set values
+  x->set(0, std::make_shared<Value>(2.5));
+  x->set(1, std::make_shared<Value>(-3.7));
+  x->set(2, std::make_shared<Value>(2.35));
+
+  y->set(0, std::make_shared<Value>(0));
+  y->set(1, std::make_shared<Value>(1));
+  y->set(2, std::make_shared<Value>(0));
+
+  std::shared_ptr<Value> out = cross_entropy(x, y);
+
+  double expectedValue = 6.8220; // calculated using pytorch nn.CrossEntropyLoss
+  double tolerance = 0.0001;    // Compare up to 4 decimal places
+
+
+  EXPECT_NEAR(out->data, expectedValue, tolerance);
+}
+
+TEST(LossTest, CrossEntropyLossTest3) {
+  std::shared_ptr<Tensor> x = std::make_shared<Tensor>(std::vector<int>{3});
+  std::shared_ptr<Tensor> y = std::make_shared<Tensor>(std::vector<int>{3});
+
+  // set values
+  x->set(0, std::make_shared<Value>(2.5));
+  x->set(1, std::make_shared<Value>(-3.7));
+  x->set(2, std::make_shared<Value>(2.35));
+
+  y->set(0, std::make_shared<Value>(0));
+  y->set(1, std::make_shared<Value>(0));
+  y->set(2, std::make_shared<Value>(1));
+
+  std::shared_ptr<Value> out = cross_entropy(x, y);
+
+  double expectedValue = 0.7720; // calculated using pytorch nn.CrossEntropyLoss
   double tolerance = 0.0001;    // Compare up to 4 decimal places
 
 
